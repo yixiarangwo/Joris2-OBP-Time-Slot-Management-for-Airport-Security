0ff2b297eb774c59a41c97a1af7a1a91aaa2f16a
yixiarangwo <94918581+yixiarangwo@users.noreply.github.com>
2024-01-23 19:59:08 +0100

    Add files via upload

---
+++ callbacks.py
@@ -0,0 +1,182 @@
+﻿from dash import Dash, dcc, html, Input, Output, State, callback, ctx,dash_table
+from latex2sympy2 import latex2sympy
+import plotly.figure_factory as ff
+import numpy as np
+
+from functions import *
+
+from simulation import *
+
+############################################ Importables tab ############################################
+# Combined callback for preventing multiple outputs of simulated service times.
+# Callback is called if arrival data is uploaded or if submit button for service dist is pressed.
+# Available arrival data will be converted to pandas dataframe and stored.
+# If both arrival data and service distribution are known, service times are sampled.
+@callback(
+    Output({"section": "intermediate", "type": "dataframe", "index": "arrivals"},    "data"),
+    Output({"section": "intermediate", "type": "dataframe", "index": "sim-service"}, "data"),
+    
+    Input({ "section": "importables",  "type": "upload",    "index": "arrivals"},    "contents"),
+    Input({ "section": "importables",  "type": "button",    "index": "server-dist"}, "n_clicks"),
+    State({ "section": "importables",  "type": "input",     "index": "server-dist",  "info": "formula"}, "value"),
+    prevent_initial_call = True
+)
+def serviceTimes(contents, n_clicks, latexExpression):
+    # Parse content to dataframe of arrivals if triggered by file upload
+    trigger_id = ctx.triggered_id
+    if contents is not None and trigger_id["index"] == "arrivals":
+        arrivals = parseContents(contents)          # ADD SHOWING FILE NAME AS CONFIRMATION OF UPLOADING (in separate callback)
+        
+    # Sample from given distribution if server dist. and arrival data given
+    simService = ''
+    #if latexExpression != '' and arrivals is not None:
+    #    samples = arrivals.shape[0]
+    #    npFunc = latexToNumpy(latexExpression)
+    #    simService = 0
+
+    return arrivals, simService
+
+# Uploading flight schedule and converting to pandas dataframe
+@callback(
+    Output({"section": "intermediate", "type": "dataframe", "index": "flights"}, "data"),
+    Input({ "section": "importables",  "type": "upload",    "index": "flights" }, "contents"),
+    prevent_initial_call = True
+) 
+def storeFlights(contents):
+    if contents is None:
+        raise dash.exceptions.PreventUpdate
+    return parseContents(contents, "records")   # ADD SHOWING FILE NAME AS CONFIRMATION OF UPLOADING
+
+# Uploading lane availability data and converting to pandas dataframe
+@callback(
+    Output({"section": "intermediate", "type": "dataframe", "index": "lanes"}, "data"),
+    Input({ "section": "importables",  "type": "upload",    "index": "lanes"}, "contents"),
+    prevent_initial_call = True
+) 
+def storeLanes(contents):
+    if contents is None:
+        raise dash.exceptions.PreventUpdate
+    return parseContents(contents)          # ADD SHOWING FILE NAME AS CONFIRMATION OF UPLOADING
+
+# Update LaTeX version of inputted distribution live
+@callback(
+    Output({"section": "importables", "type": "text",  "index": "server-dist"}, "children"),
+    Input({ "section": "importables", "type": "input", "index": "server-dist", "info": "formula"}, "value"),
+    State({ "section": "importables", "type": "text",  "index": "server-dist"}, "children")
+)
+def showLaTeX(latexExpression, prevExpression):
+    # Check if no expression
+    if latexExpression == '':
+        return ''
+    
+    # Make complete LaTeX math expression
+    latexMath = "$$f_S(x) = " + latexExpression + "$$"
+
+    # Check if correct LaTeX:
+    try:
+        latex2sympy(latexExpression)
+        return latexMath
+    except Exception as e:
+        return prevExpression
+
+# Update plot of sampled service times when dataframe changed
+#@callback(
+    #Output({"section": "importables",  "type": "graph",     "index": "sim-service"}, "figure"),
+#    Input({ "section": "intermediate", "type": "dataframe", "index": "sim-service"}, "data"),
+#    prevent_initial_call = True
+#)
+#def plotServiceTimes(simService):
+    # Set times in proper form
+    #if len(simService.shape) == 1:
+    #    simService = simService[:, np.newaxis]
+
+    # Get fig data
+    #fig = ff.create_distplot([simService], ["Security Times"])
+
+    # Set layout of plot
+  
+    #return None #fig
+
+
+############################################ Inputs tab ############################################
+@callback(
+    Output({"section": "inputs",       "type": "table",     "index": "time-slots"}, "data"),
+    Output({"section": "inputs",       "type": "table",     "index": "time-slots"}, "editable"),
+    Input({ "section": "intermediate", "type": "dataframe", "index": "flights"},    "data"),
+    prevent_initial_call = True
+)
+def timeSlotRows(flights):
+    data = [record.copy() for record in flights]
+    [record.update({"TimeSlotCapacity": "", "TimeSlotIntervals": ""}) for record in data]
+    return data, True
+
+
+# use MATCH (or the other one) to get all time slots in one go upon callback when submit button is pressed
+
+
+############################################ Analysis tab ############################################
+@callback(
+    Output('average-waiting-time-output', 'children'),
+    Output('average-Virtua-waiting-time-output', 'children'),
+    Output('average-Normal-waiting-time-output', 'children'),
+    Output('waiting-time-plot-output', 'figure'),
+    Input('run-simulation-button', 'n_clicks'),
+    State({"section": "intermediate", "type": "dataframe", "index": "arrivals"}, "data"),
+    State({"section": "intermediate", "type": "dataframe", "index": "lanes"}, "data"),
+    State({"section": "intermediate", "type": "dataframe", "index": "flights"}, "data"),
+    prevent_initial_call = True
+)
+def update_dashboard(n_clicks, passengers_data_json, security_data_json,flights_data_json):
+    if n_clicks is None:
+        raise dash.exceptions.PreventUpdate
+
+    
+    passengers_data = pd.DataFrame(passengers_data_json)
+    security_data = pd.DataFrame(security_data_json)
+    flights_data =pd.DataFrame(flights_data_json)
+    passengers_data = pd.merge(passengers_data, flights_data, left_on='FlightNumber', right_on='FlightNumber', how='left')
+    passengers_data = process_virtual_queue(passengers_data, virtual_queue).sort_values(by='new_ArrivalTime')
+    
+    #run simulation
+    simulation_result_df = multiple_run_simulation(5,40000, passengers_data, security_data)
+    print(simulation_result_df)
+    simulation_result_df["Time in hour"] = simulation_result_df.index/3600
+    non_zero_values = simulation_result_df[simulation_result_df['Average Waiting Time'] != 0]
+    non_zero_count = non_zero_values['Average Waiting Time'].count()
+    average_waiting_time = non_zero_values['Average Waiting Time'].sum() / non_zero_count
+    average_waiting_time_str = f"Average Waiting Time: {average_waiting_time:.2f} seconds"
+    
+    non_zero_values_Virtual = simulation_result_df[simulation_result_df['Average Virtual Queue Waiting Time'] != 0]
+    non_zero_count_Virtual = non_zero_values['Average Virtual Queue Waiting Time'].count()
+    average_Virtual_waiting_time= simulation_result_df['Average Virtual Queue Waiting Time'].sum()/non_zero_count
+    average_Virtual_waiting_time_str = f"Average Virtual Waiting Time: {average_Virtual_waiting_time:.2f} seconds"
+    
+    non_zero_values_Normal = simulation_result_df[simulation_result_df['Average Normal Queue Waiting Time'] != 0]
+    non_zero_count_Normal = non_zero_values['Average Normal Queue Waiting Time'].count()
+    average_Normal_waiting_time=simulation_result_df['Average Normal Queue Waiting Time'].sum()/non_zero_count
+    average_Normal_waiting_time_str =  f"Average Normal Waiting Time: {average_Normal_waiting_time:.2f} seconds"
+    
+    plot_fig = create_time_interval_plot(simulation_result_df)
+
+    return average_waiting_time_str,average_Virtual_waiting_time_str,average_Normal_waiting_time_str,plot_fig
+
+
+@callback(
+    Output('arrival-time-distribution-plot', 'figure'),
+    Input({"section": "intermediate", "type": "dataframe", "index": "arrivals"}, "data"),
+    prevent_initial_call=True
+)
+def update_arrival_time_plot(arrival_data):
+    try:
+        if not arrival_data:
+            return {}
+
+        arrival_df = pd.DataFrame(arrival_data)
+        
+        fig = create_plotly_histogram(arrival_df)
+        return fig
+    except Exception as e:
+        print(f"Error in update_arrival_time_plot: {e}")
+        return {}
+
+    
---
+++ functions.py
@@ -0,0 +1,134 @@
+﻿import io
+import base64
+import numpy as np
+import pandas as pd
+import sympy as sp
+from latex2sympy2 import latex2sympy
+from dash import Dash, dcc, html, Input, Output, callback
+import plotly.express as px
+import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
+import plotly.tools as tls
+import plotly.graph_objs as go
+############################################ Importables tab ############################################
+# Function to parse content from csv-file to dataframe
+def parseContents(contents, structure = "list"):#, fileName):
+    # Decode content
+    contents_type, contents_string = contents.split(',')
+    decoded = base64.b64decode(contents_string)
+    
+    # Assume that the user uploaded a CSV file
+    try:
+        df = pd.read_csv(io.StringIO(decoded.decode("utf-8")))
+        json = df.to_dict(orient = structure)
+        return json#, fileName
+    except Exception as e:
+        print(e)
+        return ''#, "Error!"
+
+# Function to parse LaTeX expression to numpy function, ADD LOWER AND UPPER BOUND
+def latexToNumpy(latexExpression):
+	try:
+		sympFunc = latex2sympy(latexExpression)
+		numpFunc = sp.lambdify('x', sympFunc, "numpy")
+		return np.vectorize(numpFunc)
+	except Exception as e:
+		print(e)
+		return html.Div(["There was an error processing the LaTeX equation"])
+
+
+############################################ Inputs tab ############################################
+
+############################################Analysis tab##########################################
+
+import plotly.graph_objs as go
+import numpy as np
+
+def create_plotly_histogram(arrival_df):
+    # 将 ArrivalTime 从秒转换为小时
+    arrival_times_in_hours = arrival_df['ArrivalTime'] / 3600
+
+    # 创建直方图
+    hist_data = go.Histogram(
+        x=arrival_times_in_hours,
+        nbinsx=24,  # 将一天分为24个小时
+        name='Histogram',
+        marker=dict(
+            color='blue',
+            line=dict(
+                color='black',
+                width=2
+            )
+        ),
+        opacity=0.75
+    )
+
+    # 创建图表对象
+    fig = go.Figure(data=[hist_data])
+
+    # 更新图表布局
+    fig.update_layout(
+        title_text='Distribution of Passenger Arrival Times',  # 标题
+        xaxis_title_text='Time of Day (Hours)',  # X轴标题
+        yaxis_title_text='Number of Passengers',  # Y轴标题
+        bargap=0.2,  # 直方图条间距
+        xaxis=dict(
+            tickmode='linear',
+            tick0=0,
+            dtick=1,  # 每1小时一个刻度
+        ),
+        plot_bgcolor='white'  # 背景颜色
+    )
+
+    # 添加网格线
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey')
+
+    return fig
+
+import plotly.graph_objs as go
+
+import plotly.graph_objs as go
+
+def create_time_interval_plot(df):
+    fig = go.Figure()
+
+    # 添加第一个时间序列
+    fig.add_trace(go.Scatter(
+        x=df['Time in hour'],
+        y=df['Average Waiting Time'],
+        mode='markers+lines',
+        name='Average Waiting Time',  # 用于图例的标签
+        marker=dict(color='blue', size=7)
+    ))
+
+    # 添加第二个时间序列
+    fig.add_trace(go.Scatter(
+        x=df['Time in hour'],
+        y=df['Average Virtual Queue Waiting Time'],
+        mode='markers+lines',
+        name='Average Virtual Queue Waiting Time',
+        marker=dict(color='red', size=7)
+    ))
+
+    # 添加第三个时间序列
+    fig.add_trace(go.Scatter(
+        x=df['Time in hour'],
+        y=df['Average Normal Queue Waiting Time'],
+        mode='markers+lines',
+        name='Average Normal Queue Waiting Time',
+        marker=dict(color='green', size=7)
+    ))
+
+    # 更新图表布局
+    fig.update_layout(
+        title='Average Waiting Time at Security Check (per 15-minute Interval)',
+        xaxis_title='Time Interval',
+        yaxis_title='Average Waiting Time (second)',
+        xaxis=dict(tickangle=45),
+        margin=dict(l=20, r=20, t=20, b=20),
+        paper_bgcolor="LightSteelBlue",
+        showlegend=True  # 显示图例
+    )
+
+    return fig
